47d1514cc8bdb8a6ec90228ab355063d1bdc47f2
fabioalvesrosa <faabioalvesrosa@gmail.com>
2025-02-20 00:44:34 -0300

    autentiacao, login, logout

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-from flask_login import UserMixin
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user
 
 
 #instancia por padrão
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "minha_chave_123"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce.db'
 
+login_manager = LoginManager()
 db = SQLAlchemy(app)
+login_manager.init_app(app)
+login_manager.login_view = 'login'
 CORS(app)
 
 """
@@ -31,7 +35,31 @@ class Product(db.Model):
     description = db.Column(db.Text, nullable=True)
 # para criar a base de dados ->abri terminal python -m flask shell > db.creat_all() >db.session.commit()
 
+# autenticação
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+@app.route('/login', methods=["POST"])
+def login():
+    data = request.json # receber os dados
+
+    user = User.query.filter_by(username=data.get("username")).first()
+
+    if user and data.get("password") == user.password:
+            login_user(user)
+            return jsonify({"messege": "Logged in Successfully!"})
+    
+    return jsonify({"messege": "Unauthorized. Invalid credentials"}), 401
+
+@app.route('/logout', methods=["POST"])
+@login_required
+def logout():
+    logout_user()
+    return jsonify({"messege": "Logout in Successfully!"})
+
 @app.route('/api/products/add', methods=["POST"])  #methods se nao colocar ele nao vai limitar, mas e uma boa pratica coloca
+@login_required
 def add_product():
     data = request.json
     if 'name' in data and 'price' in data:
@@ -42,6 +70,7 @@ def add_product():
     return jsonify ({"message": "Invalid product data"}), 400
 
 @app.route('/api/products/delete/<int:product_id>', methods=["DELETE"])
+@login_required
 def delete_product(product_id):
     # recuperar o produto da base de dados
     # verificar se o produto existe
@@ -67,6 +96,7 @@ def get_product_details(product_id):
         return jsonify ({"messege": "Product not found"}), 404
 
 @app.route('/api/products/update/<int:product_id>', methods=["PUT"])
+@login_required
 def update_product(product_id):
     product = Product.query.get(product_id)
     if not product:
